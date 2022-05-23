cf3e3bede4c82e155ed103c865872c1c8be7d16a
AmitrajitDas <amitrajitdas31@gmail.com>
2022-05-23 20:20:27 +0530

    feat: layouts

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import Header from '../Header'
+import Footer from '../Footer'
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
